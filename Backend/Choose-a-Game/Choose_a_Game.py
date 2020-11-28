d0925016104c4a498d571cb135814d1a747cb5b7
jamespellis2 <53198493+jamespellis2@users.noreply.github.com>
2020-11-28 12:13:30 +0000

    send name in url

--- Backend/Choose-a-Game/Choose_a_Game.py
+++ Backend/Choose-a-Game/Choose_a_Game.py
@@ -1,11 +1,23 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/games', methods=['GET'])
+def api_name():
+    if 'name' in request.args:
+        name = request.args['name']
+    else:
+        return "Error: No name provided. Please specify a name."
+
+    return name;
+
+    #return jsonify(books)
 
 app.run()
